165b8eadafab90fc78eee9d91b04bfec730ca62f
Başak Tepe <basaktepe23@gmail.com>
2026-01-03 01:50:51 +0300

    Set up Convex

--- components/providers.tsx
+++ components/providers.tsx
@@ -2,9 +2,11 @@
 
 import { ThemeProvider, useTheme } from 'next-themes'
 import { Toaster } from '@/components/ui/sonner'
+import { ConvexClientProvider } from './ConvexClientProvider'
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
+    <ConvexClientProvider>
     <ThemeProvider
       enableSystem
       attribute='class'
@@ -14,6 +16,8 @@ export default function Providers({ children }: { children: React.ReactNode }) {
       {children}
       <ToasterProvider />
     </ThemeProvider>
+    </ConvexClientProvider>
+
   )
 }
 
